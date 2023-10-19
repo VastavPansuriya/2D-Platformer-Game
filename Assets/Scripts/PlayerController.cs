4678faaf740f655da303a2ccc23dbe44bc794db3
DishaPansuriya <105767256+DishaPansuriya@users.noreply.github.com>
2023-10-19 10:02:50 +0530

    PlayerMovement
    Apply All Movement Settings To the Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
     [SerializeField] private float speed = 0;
-    [SerializeField] private float moveValue = 0;
+
+    [SerializeField] private float jumpForce = 0;
 
     private SpriteRenderer playerSprite_R;
+
     private Animator playerAnimator;
+
+    private float moveValue = 0;
+
+    private Rigidbody2D playerRb;
+
+
     // Start is called before the first frame update
     private void Start()
     {
         playerSprite_R = GetComponent<SpriteRenderer>();
         playerAnimator = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -30,7 +37,14 @@ public class PlayerController : MonoBehaviour
             //sprite flip approach1 scale 
             transform.localScale = Vector3.one;
             //sprite flip approach2 flip value
+<<<<<<< Updated upstream
     
+=======
+            //playerSprite_R.flipX = false;
+
+
+
+>>>>>>> Stashed changes
         }
 
         if (moveValue < 0)
@@ -41,7 +55,45 @@ public class PlayerController : MonoBehaviour
             //playerSprite_R.flipX = true;
         }
 
+<<<<<<< Updated upstream
         playerAnimator.SetFloat("Speed",Mathf.Abs(moveValue));
 
+=======
+       
+
+    }
+
+    private void FixedUpdate()
+    {
+        float finalMoveValue = moveValue * Time.fixedDeltaTime * speed;
+
+        playerRb.velocity = new Vector2(finalMoveValue, playerRb.velocity.y);
+    }
+
+    private void Crouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            playerAnimator.SetBool("IsCrouch", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("IsCrouch", false);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !playerAnimator.GetBool("IsCrouch"))
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            playerAnimator.SetBool("IsJump", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            playerAnimator.SetBool("IsJump", false);
+
+        }
+>>>>>>> Stashed changes
     }
 }
