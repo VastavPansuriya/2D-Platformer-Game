f8b0096e083b7ade1462f409a0b97639a9165e9d
Vastav <vastavpansuriya4444@gmail.com>
2023-10-27 10:00:29 +0530

    Look Unlock System Unity

--- Assets/Scripts/SceneLoadManager.cs
+++ Assets/Scripts/SceneLoadManager.cs
@@ -4,8 +4,45 @@ using UnityEngine.SceneManagement;
 public class SceneLoadManager : MonoBehaviour
 {
     [SerializeField] private int index = 0;
+<<<<<<< Updated upstream
 
     public void ChangeScene() {
         SceneManager.LoadScene(index);
+=======
+    private void Start()
+    {
+        if (TryGetComponent(out Button button))
+        {
+            button.onClick.AddListener(() => ButtonChangeScene());
+        }
+    }
+
+    private void ButtonChangeScene()
+    {
+        LevelStatus status = LevelManager.Instance.GetLevelStatus(index - 1);
+        switch (status)
+        {
+            case LevelStatus.Lock:
+                Debug.Log("LevelIsLoacked");
+                break;
+            case LevelStatus.Unloack:
+                SceneManager.LoadScene(index);
+                break;
+            case LevelStatus.Complete:
+                SceneManager.LoadScene(index);
+                break;
+            default:
+                break;
+        }
+>>>>>>> Stashed changes
+    }
+
+    public void ChangeSceneDoor()
+    {
+        LevelManager.Instance.SetLevelStatus(SceneManager.GetActiveScene().buildIndex, LevelStatus.Complete);
+
+        LevelManager.Instance.SetLevelStatus(SceneManager.GetActiveScene().buildIndex + 1, LevelStatus.Unloack);
+            
+        SceneManager.LoadScene("LevelSelect");
     }
 }
